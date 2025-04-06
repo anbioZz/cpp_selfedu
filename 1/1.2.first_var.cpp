4501081550bb74077f0888b633088c77fcc9b463
anbioZz <anbioZz@mail.ru>
2025-04-06 21:54:13 +0500

    add: 1.2. first var

---
+++ 1/1.2.first_var.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+  int age;                                      // переменная для записи возраста
+  cout << "How old are you?\n";                 // сколько тебе лет?
+  cin >> age;                                   // нужно указать возраст
+  cout << "You are " << age << " years old\n";  // результат
+  return 0;
+}
