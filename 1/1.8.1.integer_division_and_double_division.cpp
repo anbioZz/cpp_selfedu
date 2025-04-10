a331eb42d614eefd25395e994d90b5103059fc31
anbioZz <anbioZz@mail.ru>
2025-04-10 22:16:05 +0500

    add: Integer division and double division

---
+++ 1/1.8.1.integer_division_and_double_division.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 13;
+    int m = 5;
+
+    // Целочисленное деление: результат – целая часть от деления
+    int resultInt = n / m;
+    
+    // Деление с явным приведением типа для получения вещественного результата
+    double resultDouble = (double) n / m;
+
+    cout << "Результат целочисленного деления: " << resultInt << endl;
+    cout << "Результат деления с приведением типа: " << resultDouble << endl;
+
+    return 0;
+}
