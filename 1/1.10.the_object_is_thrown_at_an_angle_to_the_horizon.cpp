bb04a6829e43592545418c15496e29b73876ef0c
anbioZz <anbioZz@mail.ru>
2025-04-11 22:14:39 +0500

    add: The object is thrown at an angle to the horizon

---
+++ 1/1.10.the_object_is_thrown_at_an_angle_to_the_horizon.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    // Константа - ускорение свободного падения и число pi:
+    const double g = 9.8;
+    const double pi = 3.1415;
+
+    // Начальные и расчётные параметры задачи
+    // (скорость, угол и время полёта)
+    double v, alpha, T;
+
+    // Момент времени и координаты:
+    double t, x, y;
+
+    // Ввод параметров:
+    cout<<"Enter speed v = ";
+    cin>>v;
+    cout<<"Enter angel alpha = ";
+    cin>>alpha;
+    alpha = alpha * pi / 180;
+    T = 2 * v * sin(alpha) / g;
+    cout<<"Enter time t < "<<T<<": ";
+    cin>>t;
+    x = t * v * cos(alpha);
+    y = t * v * sin(alpha) - g * t * t /2;
+    cout<<"x = "<<x<<"\n";
+    cout<<"y = "<<y<<"\n";
+    return 0;
+}
