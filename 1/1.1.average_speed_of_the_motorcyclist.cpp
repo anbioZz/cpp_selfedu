095bd3f7f83e81502fa0c5ffe131f6fc22f87d61
anbioZz <anbioZz@mail.ru>
2025-04-12 14:10:34 +0500

    add: Average speed of the motorcyclist

---
+++ 1/1.1.average_speed_of_the_motorcyclist.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    // Параметры задачи
+    double S1, S2, t1, t2, V;
+    
+    // Ввод параметров
+    cout<<"Enter the value of S1 = ";
+    cin>>S1;
+    cout<<"Enter the value of S2 = ";
+    cin>>S2;
+    cout<<"Enter the value of t1 = ";
+    cin>>t1;
+    cout<<"Enter the value of t2 = ";
+    cin>>t2;
+    V = (S1 + S2) / (t1 + t2);
+    cout<<"Average speed = "<<V<<"\n";
+    return 0;
+}
