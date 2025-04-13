d7aad4ce0d09066fd8b7e0285d15cf2957b2ba76
anbioZz <anbioZz@mail.ru>
2025-04-13 22:30:09 +0500

    add: Multiplication by a power of two

---
+++ 1/1.16.multiplication_by_a_power_of_two.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    // Число, показатель степени и результат:
+    int number, n, result;
+
+    // Ввод пользователем числа:
+    cout<<"Enter the number = ";
+    cin>>number;
+
+    // Ввод пользователем показателя степени:
+    cout<<"Enter n = ";
+    cin>>n;
+
+    // Результат умножения:
+    result = number<<n;
+    cout<<"result = "<<result<<endl;
+    return 0;
+}
