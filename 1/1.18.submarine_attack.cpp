4b2ca288dc43ccda74f03af38db73dcf35e23a17
anbioZz <anbioZz@mail.ru>
2025-04-14 21:20:57 +0500

    add: Submarine attack

---
+++ 1/1.18.submarine_attack.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    // Параметры задачи:
+    double p, p1, P, q;
+    int n;
+
+    // Ввод параметров задачи:
+    cout << "p = "; cin >> p;
+
+    // Проверка корректности значения p:
+    (p < 0 || p > 1) ? (cout << "Wrong value!" << endl, exit(0))
+                     : ((void) (cout << "p1 = ")); cin >> p1;
+
+    // Проверка корректности значения p1:
+    (p1 < 0 || p1 > 1) ? (cout << "Wrong value!" << endl, exit(0))
+                       : ((void) (cout << "n = ")); cin >> n;
+
+    // Поиск решения:
+    q = (1 - p) * (1 - p1);
+    P = P * p1 * (1 - pow(q,n)) / (1 - q);
+    cout << "P = " << P << endl;
+    return 0;
+}
