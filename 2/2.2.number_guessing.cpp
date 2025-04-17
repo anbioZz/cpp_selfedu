5944fe6fd67246104b557362fbd911dabce936d1
anbioZz <anbioZz@mail.ru>
2025-04-17 23:01:10 +0500

    add: Number guessing (upgraded version)

---
+++ 2/2.2.number_guessing.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstdlib>    // for rand()
+#include <ctime>      // for time()
+using namespace std;
+int main() {
+    // Initialize the random number generator
+    srand(static_cast<unsigned>(time(nullptr))); // seed srand() with current time
+
+    int secret_number = rand() % 100 + 1;          // generate a number between 1 and 100
+    int guess;
+
+    cout << "Enter your guess (1–100): ";
+    cin >> guess;
+
+    cout<<" Secret number = "<<secret_number<<" : ";
+
+    if (guess > secret_number) {
+        cout << "Too high!" << endl;
+    }
+    else if (guess < secret_number) {
+        cout << "Too low!" << endl;
+    }
+    else {
+        cout << "Correct! You guessed the number." << endl;
+    }
+
+    return 0;
+}
